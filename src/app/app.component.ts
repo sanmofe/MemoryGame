7cf3f75dad8ab6fe5a81b2eab73f0f45f9edf75a
sanmofe <santiagomorafelix@gmail.com>
2024-05-21 12:06:00 -0500

    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Card } from './Card';
+import cards from './cards.json';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
+
+  ngOnInit() {
+    cards.cards.forEach(element => {
+      this.cardsTyped.push(element);
+    });
+  }
+
   title = 'MemoryGame';
+  cardsTyped: Card[] = [];
+
+  revealCard(card: Card) {
+    card.revealed = true;
+  }
+
 }
